8045bb6bd579c4ca460e362cc22c6ec8313d9e1f
gkonen <guillaumekonen@gmail.com>
2025-12-02 22:45:30 +0100

    Creation projet python
    creation base projet python

---
+++ BackEnd/app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+name = os.getenv("DB_NAME")
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    print(f"Database name: {name}")
+    app.run()
