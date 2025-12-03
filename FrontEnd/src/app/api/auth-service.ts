b2c2fda490da864d15b3a893d2c79ef17bc9a935
gkonen <guillaumekonen@gmail.com>
2025-12-03 14:32:11 +0100

    Basics logic for login

---
+++ FrontEnd/src/app/api/auth-service.ts
@@ -0,0 +1,17 @@
+import {Injectable, signal} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  readonly isAuthenticated = signal(false);
+
+  login(username: string, password: string) {
+    this.isAuthenticated.set(true);
+    return true
+  }
+
+  logout() {
+    this.isAuthenticated.set(false);
+  }
+}
