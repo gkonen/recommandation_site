628dd5a0329770ce5e1bbc88b948cc1df3684e00
gkonen <guillaumekonen@gmail.com>
2025-12-07 22:06:53 +0100

    Add card-movie

--- FrontEnd/src/app/page/Catalogue/catalogue.ts
+++ FrontEnd/src/app/page/Catalogue/catalogue.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {CardFilm} from '../../component/card-film/card-film';
+import {Movie} from '../../api/MovieModel';
 
 @Component({
   selector: 'app-catalogue',
-  imports: [],
+  imports: [
+    CardFilm
+  ],
   templateUrl: './catalogue.html',
   styleUrl: './catalogue.scss',
 })
 export class Catalogue {
-
+  readonly movies = signal<Movie>({
+    id: 1,
+    title: "The Matrix",
+    year: 1999,
+    rating: 4.7,
+    genres: ["Sci-Fi", "Action"],
+    tags: ["must-watch"]
+  });
 }
