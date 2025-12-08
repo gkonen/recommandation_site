17b4b8c194487c9b15010c1d168ffd3188ce9371
gkonen <guillaumekonen@gmail.com>
2025-12-08 15:03:36 +0100

    logic with pagination and end pagination component

--- FrontEnd/src/app/component/pagination/pagination.ts
+++ FrontEnd/src/app/component/pagination/pagination.ts
@@ -1,4 +1,4 @@
-import {Component, input, signal} from '@angular/core';
+import {Component, input, output, signal} from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -9,20 +9,22 @@ import {Component, input, signal} from '@angular/core';
 export class Pagination {
 
   readonly nbPages = signal<number>(1);
-
-  readonly currentPage = signal<number>(1);
+  readonly currentPage = input.required<number>();
+  readonly changePage = output<number>()
 
   ngOnInit() {
     this.nbPages.set(10);
   }
 
   goToPage(page: number) {
-    this.currentPage.set(page);
+    this.changePage.emit(page);
   }
+
   onPreviousClick() {
     if (this.currentPage() == 1) return;
     this.goToPage(this.currentPage() - 1)
   }
+
   onNextClick() {
     if (this.currentPage() == this.nbPages()) return;
     this.goToPage(this.currentPage() + 1)
