19ea308ec011481a2f5d910ef146b64c6fde0415
elena-if3 <elena.rottin@gmail.com>
2025-12-10 10:22:37 +0100

    title, year & genre filters - OK (tested in Postman)

--- BackEnd/repository/movie_repository.py
+++ BackEnd/repository/movie_repository.py
@@ -6,17 +6,13 @@ class MovieRepository:
     def __init__(self, session):
         self.session = session
 
-    # def get_all_movies(self):
-    #     movies_list = self.session.query(Movie).all()
-    #     return movies_list
-
-    def get_movie(self, movie_id):
+    def get_movie(self, movie_id) -> Movie:
         movie = self.session.query(Movie).filter(Movie.movie_id == movie_id).first()
         return movie
     
     def filter_movies(self, title=None, year=None, genre_name=None) -> list[Movie]:
         """
-        Filter movies by title, year and/or genre.
+        Filter movies by title, year and/or genre. If no filters provided, returns all movies.
         
         Args:
             title: Optional string for filtering by movie title (partial match, case-insensitive)
@@ -35,6 +31,6 @@ class MovieRepository:
             query = query.filter(Movie.year == year)
         
         if genre_name is not None:
-            query = query.join(Movie.genres).filter(Genre.genre_name == genre_name)
+            query = query.join(Movie.genres).filter(Genre.genre_name.ilike(f"%{genre_name}%"))
         
         return query.distinct().all()
