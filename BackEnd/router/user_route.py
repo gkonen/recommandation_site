8b2cd0c92b341ca2de28d75fedb506f32c67af07
elena-if3 <elena.rottin@gmail.com>
2025-12-10 14:58:04 +0100

    user login route

--- BackEnd/router/user_route.py
+++ BackEnd/router/user_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from utils.decorators import with_session
 from utils.user_factory import UserFactory
 
@@ -13,3 +13,31 @@ def get_users(controller):
 @with_session(factory_func=UserFactory.get_controller)
 def get_user(controller, user_id):
     return controller.get_user(user_id)
+
+@user_route.route('/login', methods=['POST'])
+@with_session(factory_func=UserFactory.get_controller)
+def login(controller):
+    """
+    Authenticate a user.
+    
+    Expected JSON body:
+    {
+        "username": "jane_doe",
+        "password": "secret123"
+    }
+    
+    Returns:
+    {
+        "logged": true|false,
+        "id": user_id|null
+    }
+    """
+    data = request.get_json()
+    
+    if not data:
+        return {"logged": False, "id": None, "error": "No data provided"}, 400
+    
+    username = data.get('username')
+    password = data.get('password')
+    
+    return controller.authenticate_user(username, password)
