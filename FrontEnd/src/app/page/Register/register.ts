4e5b4a5640eaf4b30f24c86895ce0fadf6fa63e9
elena-if3 <elena.rottin@gmail.com>
2025-12-11 15:52:00 +0100

    registration component work-in-progress

--- FrontEnd/src/app/page/Register/register.ts
+++ FrontEnd/src/app/page/Register/register.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RegistrationService } from '../../api/service/registration-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
+  private regService = inject(RegistrationService);
+  private router = inject(Router);
 
+  username: string = '';
+  password: string = '';
+
+  async onSubmit() {}
 }
